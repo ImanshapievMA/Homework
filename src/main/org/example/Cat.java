a6cb627bb5ddb5d882d806ce33a82bde18ab1f1d
ImanshapievMA <magulik99@gmail.com>
2024-11-16 18:56:56 +0300

    Initial commit

---
+++ src/main/org/example/Cat.java
@@ -0,0 +1,11 @@
+package main.org.example;
+
+public class Cat {
+    public static void main(String[] args) {
+        System.out.println("Dagestan gangster");
+
+        System.out.println("Dagestan");
+    }
+}
+
+
---
+++ src/src/main/Cat/Cat.java
@@ -0,0 +1,59 @@
+package src.main.Cat;
+
+public class Cat {
+    private static String year1;
+
+    public static void main(String[] args) {
+        System.out.println ("Dagestan, Dagestan, Dagestan");
+        System.out.println("Priora, Priora,Priora");
+        int Java = 30;
+        short Java2 = 15;
+        long Java3 = 20;
+
+        double Java4 = 33.5;
+        float Java5 = 44.5f;
+
+        char J = 'J';
+        boolean A = true;
+
+        byte Java6 = 110; // -128-127
+        System.out.println(Java);
+        System.out.println(Java2);
+        System.out.println(Java3);
+
+        String y = "Dagestan";
+        String space = " ";
+        String city = "Makhachkala";
+        System.out.println(y + space + city);
+        System.out.println("Dagestan " + "Kizilurt");
+        System.out.println("My number " + Java);
+
+        int value = 0;
+        while (value < 10) {
+            System.out.print("Java ");
+            value++;
+        }
+
+        for (int i = 1; i <= 10; i = i + 1){
+            System.out.println("Java " + i);
+        }
+        for (int t = 10; t >= 0; t = t - 1){
+            System.out.println("Javasya " + t);
+        }
+
+        int year = 1980;
+        while (year <= 2020) {
+            System.out.println("Olimpiada " + year + " year");
+            year = year + 4;
+        }
+        for (int ye = 1980; ye <= 2020; ye = ye + 4) {
+            System.out.println("Olimpiada " + ye + " year");
+        }
+        int k = 3;
+        for (int t = 1; t <= 9; t = t + 1){
+            System.out.println(k + "*" + t + "=" + t * k);
+
+        }
+        }
+
+    }
